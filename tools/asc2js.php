a07adbf98313689ac6409c7c4accf0c9cca93e06
Tamas Kalman <tamas@mobileplusinc.com>
2013-01-08 19:33:20 -0800

    javascript torus generated with asc2js util

--- tools/asc2js.php
+++ tools/asc2js.php
@@ -12,3 +12,11 @@ $vertexPattern = sprintf('/Vertex\ %1$s:\ *X:\ %1$s\ *Y:\ %1$s\ *Z:\ %1$s/', '([
 $facePattern = sprintf('/Face %1$s:\ *A:%1$s\ B:%1$s\ C:%1$s\ AB:%2$s BC:%2$s CA:%2$s/', '([0-9]*)', '([01]+)');
 preg_match_all($vertexPattern, $text, $vertex, PREG_SET_ORDER);
 preg_match_all($facePattern, $text, $face, PREG_SET_ORDER);
+foreach ($vertex as &$v) {
+    unset($v[0]);
+}
+foreach ($face as &$f) {
+    unset($f[0]);
+}
+printf("var vertex = %s;\n", json_encode($vertex));
+printf("var face = %s;\n", json_encode($face));
