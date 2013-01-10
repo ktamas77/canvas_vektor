9f5061e02c2b1d7af242e7011fbd4205e12e497c
Tamas Kalman <ktamas77@gmail.com>
2013-01-09 21:28:38 -0800

    javascript files moved to subfolder, 3d functions are in a separated file

---
+++ js/3d.js
@@ -0,0 +1,193 @@
+var scale = 5;              // normalize object size
+var horizontalShift = 150;  // horizontal center
+var verticalShift = 150;    // vertical center
+
+var sinMax = 1024;
+var multiplier = 32000;
+var sinA = Array();
+var cosA = Array();
+
+// -- init rotation arrays
+var pipi = (sinMax/2)/Math.PI;
+for (var i = 0;i < sinMax; i++) {
+    sinA[i] = Math.floor(Math.sin(i/(pipi))*multiplier); 
+    cosA[i] = Math.floor(Math.cos(i/(pipi))*multiplier);
+}
+
+function rotateX(vertexObj, angle)
+{   
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        sourceX = v[2];
+        sourceY = v[3];
+        sourceZ = v[4];
+        sinAngle = sinA[angle];
+        cosAngle = cosA[angle];
+        targetObj[i] = Array();
+        targetObj[i][2] = sourceX;
+        targetObj[i][3] = Math.floor(((cosAngle * sourceY) + (sinAngle * sourceZ)) / multiplier);
+        targetObj[i][4] = Math.floor(((sinAngle * sourceY) - (cosAngle * sourceZ)) / multiplier);
+    }
+    return targetObj;
+}
+
+function rotateY(vertexObj, angle)
+{   
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        sourceX = v[2];
+        sourceY = v[3];
+        sourceZ = v[4];
+        sinAngle = sinA[angle];
+        cosAngle = cosA[angle];
+        targetObj[i] = Array();
+        targetObj[i][2] = Math.floor(((cosAngle * sourceX) + (sinAngle * sourceZ)) / multiplier);
+        targetObj[i][3] = sourceY;
+        targetObj[i][4] = Math.floor(((sinAngle * sourceX) - (cosAngle * sourceZ)) / multiplier);
+    }
+    return targetObj;
+}
+
+function rotateZ(vertexObj, angle)
+{   
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        sourceX = v[2];
+        sourceY = v[3];
+        sourceZ = v[4];
+        sinAngle = sinA[angle];
+        cosAngle = cosA[angle];
+        targetObj[i] = Array();
+        targetObj[i][2] = Math.floor(((cosAngle * sourceX) + (sinAngle * sourceY)) / multiplier);
+        targetObj[i][3] = Math.floor(((sinAngle * sourceX) - (cosAngle * sourceY)) / multiplier);
+        targetObj[i][4] = sourceZ;
+    }
+    return targetObj;
+}
+
+function shiftX(vertexObj, amount)
+{
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        targetObj[i] = Array();
+        targetObj[i][2] = v[2] + amount;
+        targetObj[i][3] = v[3];
+        targetObj[i][4] = v[4];
+    }
+    return targetObj;
+}
+
+function shiftY(vertexObj, amount)
+{
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        targetObj[i] = Array();
+        targetObj[i][2] = v[2];
+        targetObj[i][3] = v[3] + amount;
+        targetObj[i][4] = v[4];
+    }
+    return targetObj;
+}
+
+function shiftZ(vertexObj, amount)
+{
+    targetObj = Array();
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        targetObj[i] = Array();
+        targetObj[i][2] = v[2];
+        targetObj[i][3] = v[3];
+        targetObj[i][4] = v[4] + amount;
+    }
+    return targetObj;
+}
+
+
+function renderVertex(vertexObj, canvasName)
+{
+    canvas = $('#' + canvasName)[0].getContext('2d');
+    canvas.fillStyle = '#00f';            
+    for (var i in vertexObj) {
+        v = vertexObj[i];
+        x = Math.round(v[2] / scale);
+        y = Math.round(v[3] / scale);
+        canvas.fillRect(x + horizontalShift, y + verticalShift, 1, 1);
+    }                
+}
+
+function renderPolygon(vertexObj, faceObj, canvasName)
+{
+    var canvas = $('#' + canvasName)[0].getContext('2d');
+
+    for (var i in faceObj) {
+        f = faceObj[i];
+        a = f[2];
+        b = f[3];
+        c = f[4];
+        vertexA = vertexObj[a];
+        vertexB = vertexObj[b];
+        vertexC = vertexObj[c];
+        x1 = vertexA[2] / scale;
+        y1 = vertexA[3] / scale;
+        x2 = vertexB[2] / scale;
+        y2 = vertexB[3] / scale;
+        x3 = vertexC[2] / scale;
+        y3 = vertexC[3] / scale;
+        canvas.beginPath();
+        canvas.fillStyle = 'rgba(' + i + ', 45, 21, 255)';
+        canvas.moveTo(x1 + horizontalShift, y1 + verticalShift);
+        canvas.lineTo(x2 + horizontalShift, y2 + verticalShift);
+        canvas.lineTo(x3 + horizontalShift, y3 + verticalShift);
+        canvas.closePath();
+        canvas.fill();
+    }               
+}
+
+function renderMesh(vertexObj, faceObj, canvasName)
+{
+    var canvas = $('#' + canvasName)[0].getContext('2d');
+    canvas.fillStyle = '#0f0';            
+    canvas.lineWidth = 1;
+    for (var i in faceObj) {
+        f = faceObj[i];
+        a = f[2];
+        b = f[3];
+        c = f[4];
+        ab = f[5];
+        bc = f[6];
+        ca = f[7];
+        vertexA = vertexObj[a];
+        vertexB = vertexObj[b];
+        vertexC = vertexObj[c];
+        x1 = vertexA[2] / scale;
+        y1 = vertexA[3] / scale;
+        x2 = vertexB[2] / scale;
+        y2 = vertexB[3] / scale;
+        x3 = vertexC[2] / scale;
+        y3 = vertexC[3] / scale;
+        if (ab) {
+            canvas.beginPath();
+            canvas.moveTo(x1 + horizontalShift, y1 + verticalShift);
+            canvas.lineTo(x2 + horizontalShift, y2 + verticalShift);
+            canvas.stroke();
+        }
+        if (bc) {
+            canvas.beginPath();
+            canvas.moveTo(x2 + horizontalShift, y2 + verticalShift);
+            canvas.lineTo(x3 + horizontalShift, y3 + verticalShift);
+            canvas.stroke();
+        }
+        if (ca) {
+            canvas.beginPath();
+            canvas.moveTo(x3 + horizontalShift, y3 + verticalShift);
+            canvas.lineTo(x1 + horizontalShift, y1 + verticalShift);
+            canvas.stroke();
+        }
+    }
+
+}
